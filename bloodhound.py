57348451e6648bdf8b1684811a8897a80a783d1d
tsmith4014 <chjthomps@gmail.com>
2024-10-14 13:15:24 -0400

    updated .py with live running lambda prior to tearing it down.  added images of aws lambda console config tabs

--- bloodhound.py
+++ bloodhound.py
@@ -1,27 +1,18 @@
+#what was deployed live on aws lambda for DevOps Bravo Platoon Cohort
 import boto3
 import os
-import json
-from dotenv import load_dotenv
-
-# Import WebClient from Python SDK (github.com/slackapi/python-slack-sdk)
 from slack_sdk import WebClient
-
-load_dotenv()
+from slack_sdk.errors import SlackApiError
 
 STUDENT_REGIONS = [
-    "eu-central-1",
-    "eu-west-1",
-    "eu-west-2",
     "us-east-1",
     "us-east-2",
     "us-west-1",
     "us-west-2",
 ]
 
-
 def create_session(region):
-    return boto3.Session(profile_name="bloodhound", region_name=region)
-
+    return boto3.Session(region_name=region)
 
 def search_regions_for_rds_resources(session):
     rds_instances = []
@@ -32,26 +23,17 @@ def search_regions_for_rds_resources(session):
         rds_instances.append(dbinstance["DBInstanceIdentifier"])
     return {"rds": rds_instances}
 
-
 def search_regions_for_ec2_resources(session):
-    """
-    TODO: modify the parsing logic here from the describe_instances() call,
-    https://serverfault.com/questions/749118/
-    Instances spun up at the same time will be in the same reservationId more than likely.
-    Instances spun up individually will have their own reservationId
-    """
     ec2_instances = []
     print(f"Sniffing out ec2 resources in {session.region_name}...")
     ec2 = session.client("ec2")
     response = ec2.describe_instances()
     try:
-        # Instances spun up at the same time will be listed in the same Reservation
         if len(response["Reservations"]) == 1:
             for ec2instance in response["Reservations"][0]["Instances"]:
                 if ec2instance["State"]["Name"] in ("stopped", "terminated"):
                     continue
                 ec2_instances.append(ec2instance["InstanceId"])
-        # Indvidual instances will appear in their own Reservation
         else:
             for ec2instance in response["Reservations"]:
                 if ec2instance["Instances"][0]["State"]["Name"] in ("stopped", "terminated"):
@@ -61,9 +43,8 @@ def search_regions_for_ec2_resources(session):
         return {"ec2": []}
     return {"ec2": ec2_instances}
 
-
 def format_message(resources):
-    message = ":dog2: Woof! Woof! :dog2:\n Bloodhound found the following resources in use: \n"
+    message = ":dog2: Woof! Woof! :dog2:\n Heads-up Bravo Platoon Bloodhound found the following resources in use: \n"
     for region in resources.keys():
         if len(resources[region]["ec2"]) == 0 and len(resources[region]["rds"]) == 0:
             continue
@@ -71,14 +52,22 @@ def format_message(resources):
     message += f"Please stop or terminate all unneeded resources!"
     return message
 
-
 def send_slack_message(message):
-    channel_id = os.environ.get("CHANNEL_ID")
-    client = WebClient(token=os.environ.get("SLACK_BOT_TOKEN"))
-    client.chat_postMessage(channel=channel_id, text=message)
+    try:
+        # Read the Slack bot token and channel ID from environment variables
+        slack_bot_token = os.environ.get("SLACK_BOT_TOKEN")
+        channel_id = os.environ.get("CHANNEL_ID")
+        
+        client = WebClient(token=slack_bot_token)
+        response = client.chat_postMessage(
+            channel=channel_id,
+            text=message
+        )
+        print(f"Slack message response: {response}")
+    except SlackApiError as e:
+        print(f"Error sending message to Slack: {e.response['error']}")
 
-
-def main():
+def lambda_handler(event, context):
     resources_in_regions = {}
     print(f"Going hunting in regions {STUDENT_REGIONS}")
     for region in STUDENT_REGIONS:
@@ -88,7 +77,102 @@ def main():
     message = format_message(resources_in_regions)
     print(message)
     send_slack_message(message)
+    return message
 
 
-if __name__ == "__main__":
-    main()
+
+#dev
+# import boto3
+# import os
+# import json
+# from dotenv import load_dotenv
+
+# # Import WebClient from Python SDK (github.com/slackapi/python-slack-sdk)
+# from slack_sdk import WebClient
+
+# load_dotenv()
+
+# STUDENT_REGIONS = [
+#     "eu-central-1",
+#     "eu-west-1",
+#     "eu-west-2",
+#     "us-east-1",
+#     "us-east-2",
+#     "us-west-1",
+#     "us-west-2",
+# ]
+
+
+# def create_session(region):
+#     return boto3.Session(profile_name="bloodhound", region_name=region)
+
+
+# def search_regions_for_rds_resources(session):
+#     rds_instances = []
+#     print(f"Sniffing out rds resources in {session.region_name}...")
+#     rds = session.client("rds")
+#     response = rds.describe_db_instances()
+#     for dbinstance in response["DBInstances"]:
+#         rds_instances.append(dbinstance["DBInstanceIdentifier"])
+#     return {"rds": rds_instances}
+
+
+# def search_regions_for_ec2_resources(session):
+#     """
+#     TODO: modify the parsing logic here from the describe_instances() call,
+#     https://serverfault.com/questions/749118/
+#     Instances spun up at the same time will be in the same reservationId more than likely.
+#     Instances spun up individually will have their own reservationId
+#     """
+#     ec2_instances = []
+#     print(f"Sniffing out ec2 resources in {session.region_name}...")
+#     ec2 = session.client("ec2")
+#     response = ec2.describe_instances()
+#     try:
+#         # Instances spun up at the same time will be listed in the same Reservation
+#         if len(response["Reservations"]) == 1:
+#             for ec2instance in response["Reservations"][0]["Instances"]:
+#                 if ec2instance["State"]["Name"] in ("stopped", "terminated"):
+#                     continue
+#                 ec2_instances.append(ec2instance["InstanceId"])
+#         # Indvidual instances will appear in their own Reservation
+#         else:
+#             for ec2instance in response["Reservations"]:
+#                 if ec2instance["Instances"][0]["State"]["Name"] in ("stopped", "terminated"):
+#                     continue
+#                 ec2_instances.append(ec2instance["Instances"][0]["InstanceId"])
+#     except IndexError:
+#         return {"ec2": []}
+#     return {"ec2": ec2_instances}
+
+
+# def format_message(resources):
+#     message = ":dog2: Woof! Woof! :dog2:\n Bloodhound found the following resources in use: \n"
+#     for region in resources.keys():
+#         if len(resources[region]["ec2"]) == 0 and len(resources[region]["rds"]) == 0:
+#             continue
+#         message += f'- *{region.upper()}*: {len(resources[region]["ec2"])} ec2 instances, {len(resources[region]["rds"])} rds instances\n'
+#     message += f"Please stop or terminate all unneeded resources!"
+#     return message
+
+
+# def send_slack_message(message):
+#     channel_id = os.environ.get("CHANNEL_ID")
+#     client = WebClient(token=os.environ.get("SLACK_BOT_TOKEN"))
+#     client.chat_postMessage(channel=channel_id, text=message)
+
+
+# def main():
+#     resources_in_regions = {}
+#     print(f"Going hunting in regions {STUDENT_REGIONS}")
+#     for region in STUDENT_REGIONS:
+#         session = create_session(region)
+#         resources_in_regions[region] = search_regions_for_ec2_resources(session)
+#         resources_in_regions[region].update(search_regions_for_rds_resources(session))
+#     message = format_message(resources_in_regions)
+#     print(message)
+#     send_slack_message(message)
+
+
+# if __name__ == "__main__":
+#     main()
